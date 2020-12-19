374bf8b34f6e9e3ba14e834673a00e602175eca1
asha joseph <ashajo183@gmail.com>
2020-12-19 10:28:50 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/asha/exercise_3.java
@@ -0,0 +1,17 @@
+package se.lexicon.asha;
+
+import java.util.Arrays;
+
+public class exercise_3 {
+    public static void main(String[] args) {
+
+        String[] stringArray =
+                { "Paris", "London", "New York", "Stockholm"
+                };
+
+        Arrays.sort(stringArray);
+
+        System.out.println(Arrays.toString(stringArray));
+
+    }
+}
